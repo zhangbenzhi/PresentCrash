1627eea62d47acfbdee29cf9db315add10a58d30
zhangbenzhi <zhangbenzhi@dangkegongyu.com>
2020-01-15 18:07:43 +0800

    no message

---
+++ present_crash/src/main/java/com/zbz/present_crash/compat/IActivityKiller.java
@@ -0,0 +1,20 @@
+package com.zbz.present_crash.compat;
+
+import android.os.Message;
+
+/**
+ * Created by wanjian on 2018/5/24.
+ */
+
+public interface IActivityKiller {
+
+    void finishLaunchActivity(Message message);
+
+    void finishResumeActivity(Message message);
+
+    void finishPauseActivity(Message message);
+
+    void finishStopActivity(Message message);
+
+
+}
